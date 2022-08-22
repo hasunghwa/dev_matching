e4c98be6c755e7c0cd37ae0af3041eb9befab1e2
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-22 23:20:07 +0900

    Fix : ProductDetail에서 state변경시 product가 누락되어 수정

--- 2021-2/src/components/ProductDetail.js
+++ 2021-2/src/components/ProductDetail.js
@@ -15,6 +15,7 @@ export default function ProductDetail({ $target, initalState }) {
 
     if (selectedOptions) {
       selectedOptions.setState({
+        product: this.state.product,
         selectedOptions: this.state.selectedOptions
       });
     };
