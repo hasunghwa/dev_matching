57891e5aa5c9a6c6e1645a95836f0163fc00b93c
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-22 22:35:58 +0900

    Feature : ProductList component 구현

---
+++ 2021-2/src/components/ProductList.js
@@ -0,0 +1,42 @@
+import { routeChange } from "../router.js";
+
+export default function ProductList({ $target, initalState }) {
+  const $productList = document.createElement('ul');
+  $target.appendChild($productList);
+  
+  this.state = initalState;
+  
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  }
+
+  this.render = () => {
+    if(!this.state) {
+      return
+    }
+    $productList.innerHTML = `
+      ${this.state.map(product =>
+        `
+          <li class="Product" data-product-id="${product.id}">
+            <img src="${product.imageUrl}">
+            <div class="Product__info">
+              <div>${product.name}</div>
+              <div>${product.price}~</div>
+            </div>
+          </li>
+        `
+      ).join('')}`;
+  };
+
+  this.render();
+
+  $productList.addEventListener('click', (e) => {
+    const $li = e.target.closest('li');
+    const { productId } = $li.dataset;
+    
+    if (productId) {
+      routeChange(`/products/${productId}`);
+    };
+  });
+}
