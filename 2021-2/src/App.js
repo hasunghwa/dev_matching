9e592c89d4fbeae6a0aeda42c3913290b4103bd2
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-23 20:52:10 +0900

    Fix : page별 Router 연동

--- 2021-2/src/App.js
+++ 2021-2/src/App.js
@@ -1,7 +1,29 @@
+import CartPage from "./pages/CartPage.js";
+import ProductDetailPage from "./pages/ProductDetailPage.js";
+import ProdectListPage from "./pages/ProductListPage.js";
+import { init } from "./router.js";
+
 export default function App({ $target }) {
   this.route = () => {
     const { pathname } = location;
     $target.innerHtml = '';
 
+    if (pathname === '/') {
+      new ProdectListPage({ $target }).render();
+    } else if (pathname.indexOf('/products/') === 0) {
+      const [, , productId] = pathname.split('/');
+      new ProductDetailPage({
+        $target,
+        productId
+      }).render();
+    } else if (pathname === '/cart') {
+      new CartPage({ $target }).render();
+    }
+  }
+
+  init(this.route);
+
   this.route();
+
+  window.addEventListener('popstate', this.route);
 }
