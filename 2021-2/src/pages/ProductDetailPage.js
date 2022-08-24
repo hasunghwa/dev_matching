630d47de1d6f6c02db5b161d2d07a09cf7db96d4
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-08-24 14:17:10 +0900

    Feature : Product Detail 페이지 DetailComponent 연동

--- 2021-2/src/pages/ProductDetailPage.js
+++ 2021-2/src/pages/ProductDetailPage.js
@@ -1,13 +1,46 @@
+import { request } from "../api.js";
+import ProductDetail from "../components/ProductDetail.js";
+
 export default function ProductDetailPage({ $target, productId }) {
   this.state = {
-    productId
+    productId,
+    product: null
   };
+  
+  this.setState = nextState => {
+    this.state = nextState;
+    this.render();
+  }
+
   const $page = document.createElement('div');
   $page.className = 'ProductDetailPage';
 
-  $page.innerHTML = `<h1>상품 정보</h1>`;
-
   this.render = () => {
-    $target.appendChild($page);
+    if (!this.state.product) {
+      $target.innerHTML = 'Loading..';
+    } else {
+      $target.innerHTML = '';
+      $target.appendChild($page);      
+      $page.innerHTML = `<h1>${this.state.product.name} 상품 정보</h1>`;
+
+      new ProductDetail({
+        $target: $page,
+        initalState: {
+          product: this.state.product,
+          selectedOptions: []
+        }
+      });
+    }    
   }
+
+  this.fetchProduct = async () => {
+    const { productId } = this.state;
+    const product = await request(`/products/${productId}`);
+    this.setState({
+      ...this.state,
+      product
+    })
+  }
+
+  this.fetchProduct();
 }
